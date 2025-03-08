397434aceb050606421c694342e7ffbca2fa6e2a
jhinwinson <josorioc@ufpso.edu.co>
2025-03-07 20:24:12 -0500

    quiz

--- practica/src/App.java
+++ practica/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hola mundooo");
         System.out.println("hola mundooo");
-        
+        System.out.println("hola mundooo");
     }
 }
